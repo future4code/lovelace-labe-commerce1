438f4b242110f103449b9ec5b6c539059ba62d8c
jose henrique holanda cavalcanti <josehenrique.holanda1@gmail.com>
2021-07-23 15:44:17 -0400

    containerFiltro

--- src/Componets/ContainerFiltro.js
+++ src/Componets/ContainerFiltro.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 export default class ContainerFiltro extends React.Component {
+    // const filtroValorMaximo = (valor) => {
+    //     listaProdutos.map((item, index) => {
+    //         if(item.value <= valor) {
+
+    //         }
+    //     })
+    // }
     render(){
 
         // const {filtroValorMaximo, filtroValorMinimo, filtroNome} = this.props.filtro
